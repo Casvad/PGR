4fb00d235f8e5dacc6767456fa6378a29c520eab
Casvad <carlos.sanchez-v@mail.escuelaing.edu.co>
2017-10-16 15:02:00 -0500

    otra topologia

--- PgrTopo.py
+++ PgrTopo.py
@@ -15,29 +15,28 @@ class PgrTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts
+		##PCS
         PC1 = self.addHost('PC1')
         PC2 = self.addHost('PC2')
         PC3 = self.addHost('PC3')
         PC4 = self.addHost('PC4')
         PC5 = self.addHost('PC5')
         PC6 = self.addHost('PC6')
-        PC7 = self.addHost('PC7')
-        PC8 = self.addHost('PC8')
-        PC9 = self.addHost('PC9')
-        PC10 = self.addHost('PC10')
-        PC11 = self.addHost('PC11')
-        PC12 = self.addHost('PC12')
-        PC13 = self.addHost('PC13')
-        PC14 = self.addHost('PC14')
-        PC15 = self.addHost('PC15')
-        PC16 = self.addHost('PC16')
-        PC17 = self.addHost('PC17')
-        PC18 = self.addHost('PC18')
+		##Phones
+        Phone1 = self.addHost('Phone1')
+        Phone2 = self.addHost('Phone2')
+        Phone3 = self.addHost('Phone3')
+        Phone4 = self.addHost('Phone4')
+		##Laptops
+        Laptop1 = self.addHost('Laptop1')
+        Laptop2 = self.addHost('Laptop2')
+        Laptop3 = self.addHost('Laptop3')
+        Laptop4 = self.addHost('Laptop4')
 
         # Add servers
 
-        Investigacion = self.addHost('Investigacion')
-        Desarrollo = self.addHost('Desarrollo')
+        Inv = self.addHost('Inv')
+        Des = self.addHost('Des')
         Pruebas = self.addHost('Pruebas')
         
         # Add switches
@@ -45,10 +44,10 @@ class PgrTopo( Topo ):
         Core1 = self.addSwitch('Core1')
         Core2 = self.addSwitch('Core2')
 
-        Distribution1 = self.addSwitch('Distribution1')
-        Distribution2 = self.addSwitch('Distribution2')
-        Distribution3 = self.addSwitch('Distribution3')
-        Distribution4 = self.addSwitch('Distribution4')
+        Dist1 = self.addSwitch('Dist1')
+        Dist2 = self.addSwitch('Dist')
+        Dist3 = self.addSwitch('Dist3')
+        Dist4 = self.addSwitch('Dist4')
 
         Access1 = self.addSwitch('Access1')
         Access2 = self.addSwitch('Access2')
@@ -60,25 +59,25 @@ class PgrTopo( Topo ):
 
         self.addLink(Core1,Core2)
 
-        self.addLink(Core1,Distribution1)
-        self.addLink(Core1,Distribution2)
-        self.addLink(Core1,Distribution3)
-        self.addLink(Core1,Distribution4)
+        self.addLink(Core1,Dist1)
+        self.addLink(Core1,Dist2)
+        self.addLink(Core1,Dist3)
+        self.addLink(Core1,Dist4)
 
-        self.addLink(Core2,Distribution1)
-        self.addLink(Core2,Distribution2)
-        self.addLink(Core2,Distribution3)
-        self.addLink(Core2,Distribution4)
+        self.addLink(Core2,Dist1)
+        self.addLink(Core2,Dist2)
+        self.addLink(Core2,Dist3)
+        self.addLink(Core2,Dist4)
 
-        self.addLink(Distribution1,Access1)
-        self.addLink(Distribution2,Access2)
-        self.addLink(Distribution3,Access3)
-        self.addLink(Distribution4,Access4)
+        self.addLink(Dist1,Access1)
+        self.addLink(Dist2,Access2)
+        self.addLink(Dist3,Access3)
+        self.addLink(Dist4,Access4)
 
         ##Between Switch-Server
 
-        self.addLink(Access4,Investigacion)
-        self.addLink(Access4,Desarrollo)
+        self.addLink(Access4,Inv)
+        self.addLink(Access4,Des)
         self.addLink(Access4,Pruebas)
 
         ##Between Switch-Host
@@ -86,22 +85,18 @@ class PgrTopo( Topo ):
         self.addLink(Access1,PC1)
         self.addLink(Access1,PC2)
         self.addLink(Access1,PC3)
-        self.addLink(Access1,PC4)
-        self.addLink(Access1,PC5)
-        self.addLink(Access1,PC6)
+        self.addLink(Access1,Phone1)
+        self.addLink(Access1,Phone2)
 
-        self.addLink(Access2,PC7)
-        self.addLink(Access2,PC8)
-        self.addLink(Access2,PC9)
-        self.addLink(Access2,PC10)
-        self.addLink(Access2,PC11)
-        self.addLink(Access2,PC12)
+        self.addLink(Access2,Laptop1)
+        self.addLink(Access2,Laptop2)
+        self.addLink(Access2,PC4)
+        self.addLink(Access2,PC5)
+        self.addLink(Access2,PC6)
 
-        self.addLink(Access3,PC13)
-        self.addLink(Access3,PC14)
-        self.addLink(Access3,PC15)
-        self.addLink(Access3,PC16)
-        self.addLink(Access3,PC17)
-        self.addLink(Access3,PC18)
+        self.addLink(Access3,Phone3)
+        self.addLink(Access3,Phone4)
+        self.addLink(Access3,Laptop3)
+        self.addLink(Access3,Laptop4)
 
 topos = { 'PgrTopo': ( lambda: PgrTopo() ) }
